958a121dc6ee1617942a677201bbd2f7e2546dca
Diyuse <daniel.yan.wu@gmail.com>
2019-09-11 14:03:33 -0400

    IT's a CaT

--- gitdemo/src/gitdemo/DemoGit.java
+++ gitdemo/src/gitdemo/DemoGit.java
@@ -3,6 +3,7 @@ package gitdemo;
 public class DemoGit {
 	
 	public static void main(String args[]) {
-		System.out.println("Bear");
+		System.out.println("Bear!");
+		System.out.println("That ain't no bear, that's a cat!!");
 	}
 }
